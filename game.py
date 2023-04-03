5bc934cac8d9d7a093d989342035722d2c0aab54
Aaron Lo <alo76@my.bcit.ca>
2023-04-02 18:47:40 -0700

    Move code into separate helper functions with beastiary jsons

---
+++ game.py
@@ -0,0 +1,290 @@
+import copy
+import random
+from copy import deepcopy
+
+# identifier = key
+#     description
+#     function call
+#     number of rooms possible
+
+
+def populate_board():
+
+    example_dictionary = {"poisontrap": ("The room fills with gas, you take 1 damage!", "poisontrap()", 2, True),
+                          "healroom": ("You are bathed in a warm light, you heal 2 HP", "healroom()", 3, True),
+                          "fireroom": ("The room is on fire, you take 2 damage!", "fireroom()", 1, True)}
+    example_board = {(0, 0): 'Empty Room', (1, 0): 'Empty Room', (2, 0): 'Empty Room', (0, 1): 'Empty Room', (1, 1):
+                     'Empty Room', (2, 1): 'Empty Room', (0, 2): 'Empty Room', (1, 2): 'Empty Room', (2, 2):
+                     'Empty Room'}
+    rooms_to_populate = copy.deepcopy(example_dictionary)
+    while rooms_to_populate:
+        for every in rooms_to_populate.value[4]:
+            print(every)
+
+
+def make_board(row: int, column: int) -> dict:
+    """
+    Create a dictionary comprised of rows multiplied by columns keys with value of 'Empty room'
+
+    :param row: an integer that is equal or greater than 2
+    :param column: an integer tha is equal or greater than 2
+    :precondition: row must be an integer that is equal or greater than 2
+    :precondition: column must be an integer that is equal or greater than 2
+    :postcondition: creates a dictionary with keys of rows by columns with value of 'Empty room'
+    :return: a dictionary comprised of rows multiplied by columns keys with value of 'Empty room'
+    :raises ValueError: if number is not an integer or is less than 2
+    >>> make_board(3, 3)
+    {(0, 0): 'Empty Room', (1, 0): 'Empty Room', (2, 0): 'Empty Room', (0, 1): 'Empty Room', (1, 1): 'Empty Room', \
+(2, 1): 'Empty Room', (0, 2): 'Empty Room', (1, 2): 'Empty Room', (2, 2): 'Empty Room'}
+
+    >>> make_board(2, 2)
+    {(0, 0): 'Empty Room', (1, 0): 'Empty Room', (0, 1): 'Empty Room', (1, 1): 'Empty Room'}
+    """
+    if row < 2 or column < 2:
+        raise ValueError("Dimension must be 2 or greater")
+    return {(lateral, longitudinal): 'Empty Room' for longitudinal in range(column) for lateral in range(row)}
+
+
+def make_character() -> dict:
+    """
+    Create a character that starts at (0, 0) and has HP of 5
+
+    :postcondition: creates a character that starts at (0, 0) and has HP of 5
+    :return: a dictionary of a character that starts at (0, 0) and has HP of 5
+    >>> make_character()
+    {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 5}
+    """
+    return {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5}
+
+
+def describe_current_location(board: dict, character: dict) -> None:
+    """
+    Return the room description located in the value that matches the character's location
+
+    :param board: a dictionary with integer-only tuples as keys and a string as values
+    :param character: a dictionary containing keys of "X-coordinate" and "Y-coordinate" that are in param board
+    :precondition: board must have only integers in the tuple and a string as values
+    :precondition: character must contain two keys named "X-coordinate" and "Y-coordinate" with values in param board
+    :postcondition: returns the string description of the room the character is in
+    :return: a string describing the room the character is in
+    :raises ValueError: if character coordinate values are not in the parameter of board
+    >>> example_board = {(0, 0): 'Empty Room', (1, 0): 'Empty Room', (0, 1): 'Empty Room', (1, 1): 'Empty Room'}
+    >>> example_character = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5}
+    >>> describe_current_location(example_board, example_character)
+    Empty Room
+    You are currently at (0, 0)
+    Your HP is: 5
+
+    >>> example_board = {(0, 0): 'Empty Room', (1, 0): 'Empty Room', (0, 1): 'Empty Room', (1, 1): 'PUPPY ROOM!'}
+    >>> example_character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 2}
+    >>> describe_current_location(example_board, example_character)
+    PUPPY ROOM!
+    You are currently at (1, 1)
+    Your HP is: 2
+    """
+    if (character["X-coordinate"], character["Y-coordinate"]) not in board:
+        raise KeyError("Character is out of bounds")
+    else:
+        print(board[(character["X-coordinate"], character["Y-coordinate"])])
+        print("You are currently at (" + str(character["X-coordinate"]) + ", " + str(character["Y-coordinate"]) + ")")
+        print("Your HP is: " + str(character["Current HP"]))
+
+
+def get_user_choice() -> int:
+    """
+    Return a number between 1 and 4 for the character to move in
+
+    :postcondition: returns an integer between 1 and 4
+    :return: an integer between 1 and 4
+    """
+
+    directions = {"1": "North", "2": "East", "3": "South", "4": "West"}
+    print("Enter a number for the direction you want:")
+    for key in directions:
+        print(key + " : " + directions[key])
+    movement = input()
+    while movement not in directions:
+        print("That is not a valid numeric choice, please select from the following:")
+        for key in directions:
+            print(key + " : " + directions[key])
+        movement = input()
+    return int(movement)
+
+
+def validate_move(board: dict, character: dict, direction: int) -> bool:
+    """
+    Return a Boolean if the character can move in chosen direction
+
+    :param board: a dictionary with integer-only tuples as keys and a string as values
+    :param character: a dictionary containing keys of "X-coordinate" and "Y-coordinate"
+    :param direction: an integer between 1 and 4
+    :precondition: board must have only integers in the tuple and a string as values
+    :precondition: character must contain two keys named "X-coordinate" and "Y-coordinate"
+    :precondition: direction is an integer between 1 and 4
+    :postcondition: returns Boolean if the character can move in chosen direction
+    :return: a Boolean if the character can move in chosen direction
+    >>> example_board = {(0, 0): 'Empty Room', (1, 0): 'Empty Room', (0, 1): 'Empty Room', (1, 1): 'Empty Room'}
+    >>> example_character = {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 5}
+    >>> example_direction = 2
+    >>> validate_move(example_board, example_character, example_direction)
+    True
+
+    >>> example_board = {(0, 0): 'Empty Room', (1, 0): 'Empty Room', (0, 1): 'Empty Room', (1, 1): 'Empty Room'}
+    >>> example_character = {'X-coordinate': 0, 'Y-coordinate': 1, 'Current HP': 3}
+    >>> example_direction = 4
+    >>> validate_move(example_board, example_character, example_direction)
+    False
+    """
+    movement_value = {1: (0, -1), 2: (1, 0), 3: (0, 1), 4: (-1, 0)}
+    projected_x_axis = character["X-coordinate"] + movement_value[direction][0]
+    projected_y_axis = character["Y-coordinate"] + movement_value[direction][1]
+    if (projected_x_axis, projected_y_axis) not in board:
+        return False
+    else:
+        return True
+
+
+def move_character(character: dict, direction: int) -> None:
+    """
+    Updates the character dictionary keys of coordinate with the new coordinate values
+
+    :param character: a dictionary containing keys of "X-coordinate" and "Y-coordinate"
+    :param direction: a string of one of "North", "East", "South", or "West"
+    :precondition: character must contain two keys named "X-coordinate" and "Y-coordinate"
+    :precondition: direction is an integer between 1 and 4 inclusive
+    :postcondition: updates the associated coordinate with the new position
+    >>> example_character = {"X-coordinate": 0, "Y-coordinate": 3, "Current HP": 5}
+    >>> example_direction = 2
+    >>> move_character(example_character, example_direction)
+    >>> example_character
+    {'X-coordinate': 1, 'Y-coordinate': 3, 'Current HP': 5}
+
+    >>> example_character = {"X-coordinate": 3, "Y-coordinate": 1, "Current HP": 3}
+    >>> example_direction = 3
+    >>> move_character(example_character, example_direction)
+    >>> example_character
+    {'X-coordinate': 3, 'Y-coordinate': 2, 'Current HP': 3}
+    """
+
+    movement_value = {1: (0, -1), 2: (1, 0), 3: (0, 1), 4: (-1, 0)}
+    character["X-coordinate"] += movement_value[direction][0]
+    character["Y-coordinate"] += movement_value[direction][1]
+
+
+def check_if_goal_attained(board: dict, character: dict) -> bool:
+    """
+    Return a Boolean of whether the character is at the bottom right hand corner of board
+
+    :param board: a dictionary with integer-only tuples as keys and a string as values
+    :param character: a dictionary containing keys of "X-coordinate" and "Y-coordinate"
+    :precondition: board must have only integers in the tuple and a string as values
+    :precondition: character must contain two keys named "X-coordinate" and "Y-coordinate"
+    :postcondition: returns a Boolean stating if the character is at max value for both X and Y-coordinates
+    :return: a Boolean stating if the character is at max value for both X and Y-coordinates
+    >>> example_board = {(0, 0): 'Empty Room', (1, 0): 'Empty Room', (0, 1): 'Empty Room', (1, 1): 'Empty Room'}
+    >>> example_character = {"X-coordinate": 0, "Y-coordinate": 1, "Current HP": 3}
+    >>>
+
+    >>> example_board = {(0, 0): 'Empty Room', (1, 0): 'Empty Room', (0, 1): 'Empty Room', (1, 1): 'Empty Room'}
+    >>> example_character = {"X-coordinate": 0, "Y-coordinate": 1, "Current HP": 3}
+    """
+    x_coordinates = []
+    y_coordinates = []
+    for key in board:
+        x_coordinates.append(key[0])
+        y_coordinates.append(key[1])
+    if character["X-coordinate"] == max(x_coordinates) and character["Y-coordinate"] == max(y_coordinates):
+        return True
+    else:
+        return False
+
+
+def check_for_foes() -> bool:
+    """
+    Return a Boolean of whether the character has encountered an enemy at 25% chance
+
+    :postcondition: returns a Boolean stating if the character has run into an enemy
+    :return: a Boolean stating if the character has run into an enemy at 25% chance
+    """
+    encounter = random.randint(1, 4)
+    if encounter == 1:
+        return True
+    else:
+        return False
+
+
+def guessing_game(character: dict) -> None:
+    """
+    Return a Boolean of whether the character is at the bottom right hand corner
+
+    :param character: a dictionary containing a key named "Current HP"
+    :precondition: character must contain a key named "Current HP"
+    :postcondition: affects the character's HP according to a right or wrong guess
+    """
+    secret_number = random.randint(1, 5)
+    try:
+        guess = int(input('"MUAHAHA, enter a number between 1 and 5 inclusive!":'))
+    except ValueError:
+        print('"That isn\'t a number dummy, NOW DIE!"')
+        print("You have taken 1 damage!")
+        character["Current HP"] -= 1
+    else:
+        if guess == secret_number:
+            print('"DARN IT, YOU GOT IT I\'LL BE BACKKKKKK"')
+        else:
+            print(f'"Sorry, the number was {secret_number}, NOW DIE!"')
+            print("You have taken 1 damage!")
+            character["Current HP"] -= 1
+
+
+def is_alive(character: dict) -> bool:
+    """
+    Return a Boolean of whether the character is alive (health above zero)
+
+    :param character: a dictionary containing a key named "Current HP" with a number as the value
+    :precondition: character must contain a key named "Current HP" with a number as the value
+    :postcondition: returns a Boolean if the character is alive or not (health above zero)
+    :return: a Boolean stating if the character is alive
+    >>> example_character = {"Current HP": 5}
+    >>> is_alive(example_character)
+    True
+
+    >>> example_character = {"Current HP": 0}
+    >>> is_alive(example_character)
+    False
+    """
+    return character["Current HP"] > 0
+
+
+def game():
+    rows = 5
+    columns = 5
+    board = make_board(rows, columns)
+    character = make_character()
+    achieved_goal = False
+    while not achieved_goal and is_alive(character):
+        describe_current_location(board, character)
+        direction = get_user_choice()
+        valid_move = validate_move(board, character, direction)
+        if valid_move:
+            move_character(character, direction)
+        else:
+            print("You can't move that way!")
+        there_is_a_challenger = check_for_foes()
+        if there_is_a_challenger:
+            guessing_game(character)
+        achieved_goal = check_if_goal_attained(board, character)
+    else:
+        if not is_alive(character):
+            print("Sorry you died, GITGUD. Run the program to try again!")
+        else:
+            print("Congratulations, you beat the game!")
+
+
+def main():
+
+    game()
+
+
+if __name__ == "__main__":
+    main()
--- test_fight.py
+++ test_fight.py
@@ -1,6 +1,15 @@
+import json
+import random
 
 character = {"X-coordinate": 0, "Y-coordinate": 0, "hp": 20, "atk": 2, "def": 1, "level": 2, "potions": 3}
-slime = {"name": "Slime", "hp": 10, "atk": 2}
+enemy_list = ('slime', 'wolf', 'knight')
+
+
+def enemy_randomizer() -> str:
+    beastiary_size = len(enemy_list)
+    enemy_number = random.randrange(beastiary_size)
+    enemy_file = enemy_list[enemy_number] + ".json"
+    return enemy_file
 
 
 def is_alive(player: dict) -> bool:
@@ -22,48 +31,93 @@ def is_alive(player: dict) -> bool:
     return player["hp"] > 0
 
 
-def player_action(player):
+def attack(player: dict, enemy: dict) -> int:
+    damage = player["level"] * player["atk"]
+    enemy["hp"] -= damage
+    print(f'You have dealt {damage} damage!')
+    return 0
 
-    actions = ["1", "2", "3"]
-    choice = input("1: Attack\n2: Defend\n3: Use Potion\n")
+
+def defend(player: dict) -> int:
+    print("You steel yourself for the oncoming attack!")
+    return player["def"]
+
+
+def potion(player: dict) -> int:
+    player["potions"] -= 1
+    player["hp"] += 5
+    print(f'You drank the potion and healed 5 HP!\nYour HP is now {player["hp"]}')
+    return 0
+
+
+def enemy_attack(player: dict, enemy: dict, guard: int) -> None:
+    enemy_damage = enemy["atk"] - guard
+    player["hp"] -= enemy_damage
+    print(f'{enemy["name"]} has dealt {enemy_damage} damage!')
+
+
+def menu_print(menu: dict) -> None:
+    for every in menu:
+        print(str(every) + ":", menu[every])
+
+
+def populate_menu(move_list: list) -> dict:
+    return dict(enumerate(move_list, 1))
+
+
+def valid_battle_choice(menu: dict, choice: str):
+    if int(choice) not in menu:
+        print("That is not a valid numeric choice, please select from the following:\n")
+        return False
+    else:
+        return True
+
+
+def potion_check(player: dict, menu: dict, choice: str):
+    if choice == '3' and player["potions"] < 1:
+        print("You're out of potions! Do something else!")
+        return False
+    else:
+        return True
+
+
+def action_select(player):
+
+    test_actions = ["Attack", "Defend", "Use Potion"]
+    menu = populate_menu(test_actions)
     action = False
     while not action:
-        if choice not in actions:
-            print("That is not a valid numeric choice, please select from the following:")
-            choice = input("1: Attack\n2: Defend\n3: Use Potion")
-        elif choice == "3":
-            if player["potions"] < 1:
-                choice = input("You're out of potions! Do something else!\n1: Attack\n2: Defend\n3: Use Potion")
-            else:
-                action = True
-        else:
-            action = True
+        menu_print(menu)
+        choice = input()
+        action = valid_battle_choice(menu, choice)
+        if choice == "3":
+            action = potion_check(player, menu, choice)
     return choice
 
 
-def fight(player, enemy):
+def player_turn(choice, player, enemy):
+    if choice == "1":
+        return attack(player, enemy)
+    if choice == "2":
+        return defend(player)
+    if choice == "3":
+        return potion(player)
+
+
+def fight(player):
+
+    filename = enemy_randomizer()
+    with open(filename) as file_object:
+        enemy = json.load(file_object)
 
     print(f'You have encountered {enemy["name"]}!')
     while enemy["hp"] > 0 and is_alive(player):
-        guard = 0
         print(f'Your HP is {player["hp"]}!\nThe {enemy["name"]}\'s HP is {enemy["hp"]}!')
         print("Choose an action:")
-        choice = player_action(player)
-        if choice == "1":
-            attack = player["level"] * player["atk"]
-            enemy["hp"] -= attack
-            print(f'You have dealt {attack} damage!')
-        elif choice == "2":
-            guard = player["def"]
-            print("You steel yourself for the oncoming attack!")
-        else:
-            player["potions"] -= 1
-            player["hp"] += 5
-            print(f'You drank the potion and healed 5 HP!\nYour HP is now {player["hp"]}')
+        choice = action_select(player)
+        guard = player_turn(choice, player, enemy)
         if enemy["hp"] > 0:
-            enemy_damage = enemy["atk"] - guard
-            player["hp"] -= enemy_damage
-            print(f'{enemy["name"]} has dealt {enemy_damage} damage!')
+            enemy_attack(player, enemy, guard)
     if enemy["hp"] < 1:
         print(f'You have slain {enemy["name"]}!')
     if not is_alive(player):
@@ -72,7 +126,7 @@ def fight(player, enemy):
 
 def main():
 
-    fight(character, slime)
+    fight(character)
 
 
 if __name__ == "__main__":
