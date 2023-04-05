7a73c312cd6d7a380fc7bfff69b0e87f50a16ead
Aaron Lo <alo76@my.bcit.ca>
2023-04-04 22:20:12 -0700

    Change keys to reflect "Current HP" and player variable

--- battle/battle_actions.py
+++ battle/battle_actions.py
@@ -13,17 +13,23 @@ def attack(player: dict, enemy: dict) -> int:
 
 def defend(player: dict, _) -> int:
     print("You steel yourself for the oncoming attack!")
-    return player["def"]
+    return player["level"] * player["def"]
 
 
 def potion(player: dict, _) -> int:
     player["potions"] -= 1
-    player["hp"] += 5
-    print(f'You drank the potion and healed 5 HP!\nYour HP is now {player["hp"]}')
+    player["Current HP"] += 5
+    healed = 5
+    if player["Current HP"] > player["Max HP"]:
+        healed = 5 - (player["Current HP"] - player["Max HP"])
+        player["Current HP"] = player["Max HP"]
+    print(f'You drank the potion and healed {healed} HP!\nYour HP is now {player["Current HP"]}')
     return 0
 
 
 def enemy_attack(player: dict, enemy: dict, guard: int) -> None:
     enemy_damage = enemy["atk"] - guard
-    player["hp"] -= enemy_damage
+    if enemy_damage < 0:
+        enemy_damage = 0
+    player["Current HP"] -= enemy_damage
     print(f'{enemy["name"]} has dealt {enemy_damage} damage!')
