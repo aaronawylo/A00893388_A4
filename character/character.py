8ef5aa2784a89b972cef88233b84276836cdefef
Aaron Lo <alo76@my.bcit.ca>
2023-04-04 22:16:19 -0700

    Change "character" in is_alive to "player" for standardization

--- character/character.py
+++ character/character.py
@@ -5,9 +5,9 @@ def is_alive(player: dict) -> bool:
     Return a Boolean of whether the character is alive (health above zero)
 
     :param player: a dictionary containing a key named "Current HP" with a number as the value
-    :precondition: character must contain a key named "Current HP" with a number as the value
-    :postcondition: returns a Boolean if the character is alive or not (health above zero)
-    :return: a Boolean stating if the character is alive
+    :precondition: player must contain a key named "Current HP" with a number as the value
+    :postcondition: returns a Boolean if the player is alive or not (health above zero)
+    :return: a Boolean stating if the player is alive
     >>> example_player = {"Current HP": 5}
     >>> is_alive(example_player)
     True
