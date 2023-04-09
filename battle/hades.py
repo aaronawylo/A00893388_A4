9a73bc27365c348c6a55c48e008cc804b45f8c96
Aaron Lo <alo76@my.bcit.ca>
2023-04-09 15:42:10 -0700

    Add docstrings, doctest and unit tests for boss_laugh

--- battle/hades.py
+++ battle/hades.py
@@ -5,11 +5,32 @@ A0089388
 
 
 def boss_laugh(_, enemy: dict, ___) -> None:
-    print(f"{enemy['name']} doubles over in laughter at your feeble attempt.")
+    """
+    Hades laughs at you and does nothing. Yup that's it.
+
+    :param _: Unused.
+    :param enemy: must be a dictionary containing the key "name"
+    :param ___: Unused.
+    :precondition: enemy must be a dictionary containing the key "name"
+    :precondition: enemy's "name" key must have a string as the value denoting the name
+    :postcondition: laughs at the player
+    >>> test_player = {"Not": "Needed"}
+    >>> test_guard = {"Also": "Needed"}
+    >>> test_enemy = {"name": "Hades"}
+    >>> boss_laugh(test_player, test_enemy, test_guard)
+    Hades doubles over in laughter at your feeble attempt.
+    """
+    if "name" not in enemy:
+        raise KeyError("Read my docstrings, dummy.")
+    else:
+        print(f"{enemy['name']} doubles over in laughter at your feeble attempt.")
 
 
 def ult_charge(_, enemy: dict, ___) -> None:
-    print(f"{enemy['name']} is gathering a massive amount of fire!")
+    if "name" not in enemy:
+        raise KeyError("Read my docstrings, dummy.")
+    else:
+        print(f"{enemy['name']} is gathering a massive amount of fire!")
 
 
 def ult_move(player: dict, enemy: dict, guard: int) -> None:
@@ -17,8 +38,9 @@ def ult_move(player: dict, enemy: dict, guard: int) -> None:
         raise KeyError("Read my docstrings, dummy.")
     elif type(enemy["atk"]) != int or type(player["Current HP"]) != int or type(guard) != int:
         raise TypeError("Read my docstrings, dummy.")
-    enemy_damage = (enemy["atk"] * 10) - guard
-    if enemy_damage < 0:
-        enemy_damage = 0
-    player["Current HP"] -= enemy_damage
-    print(f'{enemy["name"]} unleashes all of his built up energy!\n{enemy["name"]} has dealt {enemy_damage} damage!')
+    else:
+        enemy_damage = (enemy["atk"] * 10) - guard
+        if enemy_damage < 0:
+            enemy_damage = 0
+        player["Current HP"] -= enemy_damage
+        print(f'{enemy["name"]} unleashes all of his built up energy!\nYou took {enemy_damage} damage!')
---
+++ tests/test_boss_laugh.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from battle.hades import boss_laugh
+from unittest.mock import patch
+import io
+
+
+class TestBossLaugh(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_boss_laugh_print(self, mock_output):
+        test_player = {"Not": "Needed"}
+        test_guard = {"Also": "Needed"}
+        test_enemy = {"name": "Hades"}
+        boss_laugh(test_player, test_enemy, test_guard)
+        actual = mock_output.getvalue()
+        expected = "Hades doubles over in laughter at your feeble attempt.\n"
+        self.assertEqual(actual, expected)
+
+    def test_boss_laugh_need_a_name(self):
+        with self.assertRaises(KeyError):
+            test_player = {"Not": "Needed"}
+            test_guard = {"Also": "Needed"}
+            test_enemy = {"Oopsies": "Poopsies"}
+            boss_laugh(test_player, test_enemy, test_guard)
